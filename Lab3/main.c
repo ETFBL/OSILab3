6d98bb81d45f58939ee36695155ca7088d31aa33
ETFBL <nickyleeyy@linuxmail.org>
2015-12-18 13:53:42 +0100

    novi ispis

---
+++ Lab3/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Student
+ *
+ * Created on December 18, 2015, 1:53 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("Lab3 OSI");
+    return (EXIT_SUCCESS);
+}
+
